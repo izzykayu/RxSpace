5607ada28553a25af22fdff2f2455b3761551d09
Allison Black <aab3711@gmail.com>
2020-03-08 16:10:56 -0400

    add write function

--- task4/prep_for_embed.py
+++ task4/prep_for_embed.py
@@ -1,16 +1,28 @@
 import sys
 import re
 import csv
+import nltk.data
 from nltk.tokenize import TweetTokenizer
 
-tweet_tokenizer = TweetTokenizer()
+sentence_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+tweet_processor = TweetTokenizer()
 
-def process(sentence):
-	sentence = re.sub("@.* ", "_U ", sentence)
-	sentence = re.sub("[0-9]", "d", sentence)
-	sentence = sentence.lower()
-	t_sentence = tweet_tokenizer.tokenize(sentence)
-	final = " ".join(t_sentence)
+
+def write_to_emb(sentence):
+	emb = open('./data/emb.txt', 'a')
+	emb.write(sentence + '\n')
+	emb.close()
+
+def process(line):
+	line = line.strip()
+	sentences = sentence_detector.tokenize(line)
+	for sentence in sentences:
+		sentence = re.sub(' @.* ', ' _U ', sentence)
+		sentence = re.sub('[0-9]', 'd', sentence) 
+		sentence = sentence.lower()
+		t_sentence = tweet_processor.tokenize(sentence)
+		final = " ".join(t_sentence)
+		write_to_emb(final)
 	return final
 
 def load_data(file_to_add, text_col_name, delimiter=','):
@@ -18,8 +30,6 @@ def load_data(file_to_add, text_col_name, delimiter=','):
 		reader = csv.DictReader(csvfile)
 		for row in reader:
 			new_text = process(row[text_col_name])
-			print(new_text)
-			break
 
 #	if file_to_add.endswith('csv'):
 #		contents = pd.read_csv(file_to_add, delimiter=delimiter)
